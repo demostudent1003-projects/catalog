7fb3a3011caa019f8a5233ecc962075bacf7f9dd
elizabeth campbell <elizabeth.lisa.campbell@gmail.com>
2017-11-03 19:45:11 +0000

    catalog
    midterm project

--- app/controllers/coffee_beans_controller.rb
+++ app/controllers/coffee_beans_controller.rb
@@ -1,12 +1,13 @@
 class CoffeeBeansController < ApplicationController
   def index
-    coffee_beans = CoffeeBean.all
+    @coffee_beans = CoffeeBean.all
 
     render("coffee_beans_templates/index.html.erb")
   end
 
   def show
-    @coffee_bean = CoffeeBean.find(params[id])
+    # Params are like this: Parameters: {"id"=>"21"}
+    @coffee_beans = CoffeeBean.find(params[:id])
 
     render("coffee_beans_templates/show.html.erb")
   end
@@ -16,12 +17,13 @@ class CoffeeBeansController < ApplicationController
   end
 
   def create_row
+    # Params are like this: Parameters: {"some_blend_name"=>"test", "some_origin"=>"test", "some_variety"=>"test", "some_notes"=>"test"}
     @coffee_bean = CoffeeBean.new
 
-    @coffee_bean.blend_name = params[:blend_name]
-    @coffee_bean.origin = params[:origin]
-    @coffee_bean.variety = params[:variety]
-    @coffee_bean.notes = params[:notes]
+    @coffee_bean.blend_name = params[:some_blend_name]
+    @coffee_bean.origin = params[:some_origin]
+    @coffee_bean.variety = params[:some_variety]
+    @coffee_bean.notes = params[:some_notes]
 
     @coffee_bean.save
 
@@ -29,25 +31,33 @@ class CoffeeBeansController < ApplicationController
   end
 
   def edit_form
+    #Params are like this: Parameters: {"id"=>"22"}
+    editing_id = params["id"]
+    s = CoffeeBean.find(editing_id)
+    @blend_name = s.blend_name
+    @origin = s.origin
+    @variety = s.variety
+    @notes = s.notes
+    @id = editing_id
     render("coffee_beans_templates/edit_form.html.erb")
   end
 
   def update_row
+    # Params are like this: Parameters: {"blend_name"=>"testing", "origin"=>"testing", "variety"=>"testing", "notes"=>"testing"}
     @coffee_bean = CoffeeBean.find(params[:id])
-
     @coffee_bean.blend_name = params[:blend_name]
     @coffee_bean.origin = params[:origin]
     @coffee_bean.variety = params[:variety]
     @coffee_bean.notes = params[:notes]
-
     @coffee_bean.save
-
     redirect_to("/coffee_beans/#{@coffee_bean.id}")
   end
 
   def destroy_row
-    @coffee_bean = CoffeeBean.find(params[:id])
-
-    @coffee_bean.destroy
+    # Params are like this: Parameters: {"id"=>"22"}
+    to_destroy_id = params["id"]
+    c = CoffeeBean.find(to_destroy_id)
+    c.destroy
+    redirect_to("/coffee_beans")
   end
 end
--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,23 @@
+require 'open-uri'
+
 class FoodsController < ApplicationController
   def index
-    @foods = Food.list
+    @foods = Food.all
 
     render("foods_templates/index.html.erb")
   end
 
   def show
+    # Params are like this: Parameters: {"id"=>"6"}
     @food = Food.find(params[:id])
-
+    @street_address = @food.store_address
+    
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@street_address}" # This is the URL we captured from the Google API; we want to vary street_address. 
+    parsed_data = JSON.parse(open(url).read) # This takes the URL, opens it and reads it, and then parses the JSON within.
+    
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"] # This returns the latitude value from the JSON.
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"] # This returns the longitude value from the JSON.
+    
     render("foods_templates/show.html.erb")
   end
 
@@ -16,16 +26,17 @@ class FoodsController < ApplicationController
   end
 
   def create_row
+    # Params are like this: Parameters: {"some_ingredient"=>"test", "some_spice"=>"test", "some_measurement"=>"test", "some_store_address"=>"test"}
     @food = Food.new
 
-    @food.ingredient = params[:ingredient]
-    @food.spice = params[:spice]
-    @food.measurement = params[:measurement]
-    @food.store_address = params[:store_address]
-
+    @food.ingredient = params[:some_ingredient]
+    @food.spice = params[:some_spice]
+    @food.measurement = params[:some_measurement]
+    @food.store_address = params[:some_store_address]
+    @food.save
     redirect_to("/foods")
   end
-
+  
   def edit_form
     @food = Food.find(params[:id])
 
@@ -42,12 +53,15 @@ class FoodsController < ApplicationController
 
     @food.save
 
-    redirect_to("/foods")
+    redirect_to("/foods/#{@food.id}")
+    
   end
 
   def destroy_row
+    # Params are like this: Parameters: {"id"=>"21"}
+    @food = Food.find(params[:id])
     @food.destroy
-
     redirect_to("/foods")
   end
+  
 end
